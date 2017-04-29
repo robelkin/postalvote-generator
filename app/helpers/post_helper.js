c6443c3d897f4b0ec21d1a9441908a20df6dffec
Rob Elkin <robelkin@gmail.com>
2017-04-29 16:08:06 +0100

    validation for date choosing

--- app/helpers/post_helper.js
+++ app/helpers/post_helper.js
@@ -12,6 +12,23 @@ module.exports = {
 
         req.checkBody('postallength', 'You must choose the length for your postal vote').notEmpty();
 
+        if(req.body.postallength == "specificdate")
+        {
+          req.checkBody('postallength-date-day', 'If you are specifying a specific postal vote date, you must specify a day').notEmpty().isInt().isLength(2);
+          req.checkBody('postallength-date-month', 'If you are specifying a specific postal vote date, you must specify a month').notEmpty().isInt().isLength(2);
+          req.checkBody('postallength-date-year', 'If you are specifying a specific postal vote date, you must specify a year').notEmpty().isInt().isLength(4);
+        }
+        else if(req.body.postallength == "period")
+        {
+          req.checkBody('postallength-period-start-day', 'If you are specifying a postal vote period, you must specify a start day').notEmpty().isInt().isLength(2);
+          req.checkBody('postallength-period-start-month', 'If you are specifying a postal vote period, you must specify a start month').notEmpty().isInt().isLength(2);
+          req.checkBody('postallength-period-start-year', 'If you are specifying a postal vote period, you must specify a start year').notEmpty().isInt().isLength(4);
+
+          req.checkBody('postallength-period-end-day', 'If you are specifying a postal vote period, you must specify an end day').notEmpty().isInt().isLength(2);
+          req.checkBody('postallength-period-end-month', 'If you are specifying a postal vote period, you must specify an end month').notEmpty().isInt().isLength(2);
+          req.checkBody('postallength-period-end-year', 'If you are specifying a postal vote period, you must specify an end year').notEmpty().isInt().isLength(4);
+        }
+
         if(req.body.ballotaddress1 != undefined)
         {
             req.checkBody('ballotaddress1', 'ballotaddress1 is required').notEmpty();
